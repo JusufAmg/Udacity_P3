cc4e0c345ce2006ff034e7a3ae5b65be52c79dd8
JusufAmg <jamgad23@gmail.com>
2020-10-19 04:58:56 +0300

    resolving_bug

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+//    delete _chatBot; deleting an alrady deleted memory location 
 
     ////
     //// EOF STUDENT CODE
